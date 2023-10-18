e0515d3283170601ef8072294916b840ef6a693c
Abdiel Brilhante <abdielbrilhante@gmail.com>
2023-10-18 08:40:15 -0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'eqeqeq': ['error', 'smart'],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-console': 'error',
+    'no-control-regex': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': 'warn',
+  },
+};
